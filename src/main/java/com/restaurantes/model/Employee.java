ff82aeef4039022b61810224251649ce6d863c21
Rubén Pinto <angel_rubenp@hotmail.com>
2026-04-14 18:56:10 +0200

    Initial commit

---
+++ src/main/java/com/restaurantes/model/Employee.java
@@ -0,0 +1,51 @@
+package com.restaurantes.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+/*
+insert into employees (nif, level) values ('11A', 'JUNIOR');
+insert into employees (nif, level) values ('22B', 'SENIOR');
+insert into employees (nif, level) values ('33C', 'INVENT');
+
+SELECT * FROM employees;
+ */
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+@AllArgsConstructor
+@ToString
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(unique = true)
+    private String nif;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    // Tipo de empleado: JUNIOR o SENIOR con enum
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "ENUM('JUNIOR', 'SENIOR') DEFAULT 'SENIOR'")
+    private WorkLevel level = WorkLevel.SENIOR;
+
+    private LocalDate startDate = LocalDate.now(); // 2026-04-13
+    // LocalDate
+    // LocalTime
+    // LocalDateTime
+
+    @ToString.Exclude // lombok
+    @ManyToOne() // JPA
+    private Restaurant restaurant;
+
+}
